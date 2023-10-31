eec5b75e2e7164521144a384327622d48a7498fb
Blake Arnsdorff <blake@dontpanicux.com>
2023-10-31 11:36:26 -0700

    Defined Global Vars
    Defined all the necessary interactive elements in JS & ensured all vars called the correct UI element.

--- index.js
+++ index.js
@@ -3,4 +3,27 @@ console.log('script linked')
 // Global Vars
 const convertInputVal = document.getElementById("convert-value")
 console.log(convertInputVal.value)
+    // lookup textChange(IDFK?) event listen to capture text input value changes
+
+const convertBtn = document.getElementById("convert-btn")
+console.log(convertBtn)
+
+// for each p element with the conversion strings
+let lengthConversion = document.getElementById("length-conversion")
+console.log(lengthConversion)
+let volumeConversion = document.getElementById("volume-conversion")
+console.log(volumeConversion)
+let massConversion = document.getElementById("mass-conversion")
+console.log(massConversion)
+
+// Unit Conversion Math
+/*
+
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+
+*/
+
+// Functionality | Interactivity
 
